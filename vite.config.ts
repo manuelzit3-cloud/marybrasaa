f8ed66f5cb6fad4b62ce56aef5b82d3cf13691ca
manuelzit3-cloud <manuelzit3@gmail.com>
2026-08-25 14:33:14 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({});
+export default defineConfig({
+  tanstackStart: {
+    server: {
+      entry: "server",
+    },
+  },
+
+  vite: {
+    base: "/marybrasaa/",
+  },
+});
