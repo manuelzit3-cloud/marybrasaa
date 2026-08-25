703289f8e6d1e7ba1f3829887f79525458aa4af9
manuelzit3-cloud <manuelzit3@gmail.com>
2026-08-25 14:28:33 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,3 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  tanstackStart: {
-    server: {
-      entry: "server",
-    },
-    prerender: {
-      enabled: true,
-      crawlLinks: true,
-      autoStaticPathsDiscovery: true,
-      autoSubfolderIndex: true,
-    },
-  },
-});
+export default defineConfig({});
